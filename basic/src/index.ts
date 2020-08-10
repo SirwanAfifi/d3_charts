d0e70a5ec84ee1beb3810a05444456a1b61b19e1
Sirwan <sir1afifi@gmail.com>
2020-08-10 20:10:11 +0430

    Add a basic bar chart

--- basic/src/index.ts
+++ basic/src/index.ts
@@ -1,8 +1,24 @@
 import * as d3 from "d3";
 
-d3.select("#wrapper")
-  .selectAll("p")
-  .data([1, 2, 3])
+const DUMMY_DATA = [
+  { id: 1, value: 50 },
+  { id: 2, value: 200 },
+  { id: 3, value: 20 },
+  { id: 4, value: 100 },
+  { id: 4, value: 100 },
+  { id: 4, value: 100 },
+];
+
+const container = d3
+  .select("#wrapper")
+  .classed("container", true)
+  .style("border", "1px solid red");
+
+container
+  .selectAll(".bar")
+  .data(DUMMY_DATA)
   .enter()
-  .append("p")
-  .text((data) => data);
+  .append("div")
+  .classed("bar", true)
+  .style("width", "50px")
+  .style("height", (data) => data.value + "px");
