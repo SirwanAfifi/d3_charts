46845634871cff85d0860262df25e2cef1dca62e
Sirwan <sir1afifi@gmail.com>
2020-08-12 14:00:00 +0100

    Change to SVG

--- basic/src/index.ts
+++ basic/src/index.ts
@@ -1,24 +1,19 @@
 import * as d3 from "d3";
 
 const DUMMY_DATA = [
-  { id: 1, value: 50 },
-  { id: 2, value: 200 },
-  { id: 3, value: 20 },
-  { id: 4, value: 100 },
-  { id: 4, value: 100 },
-  { id: 4, value: 100 },
+  { id: 1, value: 10 },
+  { id: 2, value: 11 },
+  { id: 3, value: 12 },
+  { id: 4, value: 6 },
 ];
 
-const container = d3
-  .select("#wrapper")
-  .classed("container", true)
-  .style("border", "1px solid red");
+const container = d3.select("svg").classed("container", true);
 
 container
   .selectAll(".bar")
   .data(DUMMY_DATA)
   .enter()
-  .append("div")
+  .append("rect")
   .classed("bar", true)
-  .style("width", "50px")
-  .style("height", (data) => data.value + "px");
+  .attr("width", 50)
+  .attr("height", (data) => data.value * 50);
