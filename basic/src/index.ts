dc4df62ea01f7b9982c4718021dee46d89b59a90
Sirwan <sir1afifi@gmail.com>
2020-08-13 14:00:00 +0100

    Add bar chart with SVG

--- basic/src/index.ts
+++ basic/src/index.ts
@@ -1,4 +1,5 @@
 import * as d3 from "d3";
+import * as d3Scale from "d3-scale";
 
 const DUMMY_DATA = [
   { id: 1, value: 10 },
@@ -6,6 +7,13 @@ const DUMMY_DATA = [
   { id: 3, value: 12 },
   { id: 4, value: 6 },
 ];
+
+const xScale = d3
+  .scaleBand()
+  .domain(DUMMY_DATA.map((dataPoint) => dataPoint.id.toString()))
+  .rangeRound([0, 250])
+  .padding(0.1);
+const yScale = d3.scaleLinear().domain([0, 15]).range([400, 0]);
 
 const container = d3.select("svg").classed("container", true);
 
@@ -15,5 +23,7 @@ container
   .enter()
   .append("rect")
   .classed("bar", true)
-  .attr("width", 50)
-  .attr("height", (data) => data.value * 50);
+  .attr("width", xScale.bandwidth())
+  .attr("height", (data) => 400 - yScale(data.value))
+  .attr("x", (data) => xScale(data.id.toString()))
+  .attr("y", (data) => yScale(data.value));
